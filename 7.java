3615f48b74d2ef2e9817b2dc083f756f6c7a416d
Nkosuru <nag30devops@gmail.com>
2019-06-08 22:25:10 +0530

    Added the Idea 1 to 7.java

--- 7.java
+++ 7.java
@@ -3,4 +3,8 @@ Line 2 added from Master Branch
 Line 1
 Line 2
 Added Line 3 from IOS branch
+<<<<<<< Updated upstream
 Idea 2 implemented 0n 8th June 2019
+=======
+Idea1 implemented on 8th june 2019
+>>>>>>> Stashed changes
